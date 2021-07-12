4c542b5f0e7e68f234d9d77d61fc76729a29682e
Wiktor <gejzowany@gmail.com>
2021-07-12 17:54:17 +0200

    Add extra functionality and style

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const container = document.querySelector("#container");
+const blackColor = document.querySelector("#blackColor");
+const rainbowColor = document.querySelector("#rainbowColor");
 numCells = 16;
+let cellColor = "black";
+
+blackColor.addEventListener("click", () => {
+  cellColor = "black";
+});
+rainbowColor.addEventListener("click", () => {
+  cellColor = "rainbow";
+});
+
 function createGrid(numCells) {
   for (let i = 0; i < numCells * numCells; i++) {
     let tile = document.createElement("div");
@@ -12,6 +23,7 @@ function createGrid(numCells) {
     container.appendChild(tile);
   }
 }
+
 createGrid(numCells);
 function selectSize() {
   numCells = prompt("Input size (above 0 and below 100)");
@@ -24,6 +36,23 @@ function selectSize() {
     createGrid(numCells);
   }
 }
+
+function randomNumberGen() {
+  return Math.floor(Math.random() * 256);
+}
+
 function paintTiles(e) {
-  e.target.style.backgroundColor = "black";
+  switch (cellColor) {
+    case "black":
+      e.target.style.backgroundColor = "black";
+      break;
+    case "rainbow":
+      e.target.style.backgroundColor = `rgb(${randomNumberGen()},${randomNumberGen()},${randomNumberGen()} )`;
+  }
+}
+function clearGrid() {
+  let tile = document.querySelectorAll(".tile");
+  tile.forEach((tile) => {
+    tile.style.backgroundColor = "#a3a3a3";
+  });
 }
